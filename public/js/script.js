f6806606a27f3b366610f93865e42acf0ab1f567
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2017-01-18 11:57:41 -0500

    index page complete

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 console.log("script loaded")
 
-// var scrollInterval = setInterval(function() {
-//     document.body.scrollTop = document.body.scrollHeight;
-// }, 50);
+$('[data-popup="popup-1"]').fadeIn(5000)
 
-
-
+    //----- CLOSE
+  console.log($('[data-popup-close]'));
+    $('[data-popup-close]').on('click', function(e)  {
+        var targetedPopup = $(this).attr('data-popup-close');
+        $('[data-popup="' + targetedPopup + '"]').fadeOut(1000);
+    });
+//end of dialog box//
 
 
 
